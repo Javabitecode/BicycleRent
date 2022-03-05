3785e52b04188b1ce1527904a9398408ba209221
Jababite <tro.nick1997@yandex.ru>
2022-03-05 11:28:50 +0300

    added domain/bicycle

---
+++ src/main/java/trokhimchuk/bicycle/domain/Bicycle.java
@@ -0,0 +1,52 @@
+package trokhimchuk.bicycle.domain;
+
+
+
+import javax.persistence.*;
+
+    @Entity
+    @Table
+
+    @ToString(of = {"id", "typeBicycle"})
+    @EqualsAndHashCode(of = {"id"}) //исправить
+    public class Bicycle {
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.AUTO)
+        private Long id;
+
+        private String typeBicycle;
+        @ManyToOne(fetch = FetchType.EAGER)
+        @JoinColumn(name = "user_id")
+        private User renter;
+
+        public String getAuthorName(){
+            return renter != null ? renter.getUsername() : "<none>";
+        }
+
+        public Long getId() {
+            return id;
+        }
+
+        public void setId(Long id) {
+            this.id = id;
+        }
+
+        public String getTypeBicycle() {
+            return typeBicycle;
+        }
+
+        public void setTypeBicycle(String typeBicycle) {
+            this.typeBicycle = typeBicycle;
+        }
+
+        public User getRenter() {
+            return renter;
+        }
+
+        public void setRenter(User renter) {
+            this.renter = renter;
+        }
+    }
+
+}
