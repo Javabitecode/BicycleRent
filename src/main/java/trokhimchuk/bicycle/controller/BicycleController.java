2dd8e3a17b24b88a0a5cc6b0cd9679bc4c023172
Jababite <tro.nick1997@yandex.ru>
2022-03-05 11:25:45 +0300

    added domain

--- src/main/java/trokhimchuk/bicycle/controller/BicycleController.java
+++ src/main/java/trokhimchuk/bicycle/controller/BicycleController.java
@@ -1,5 +1,7 @@
 package trokhimchuk.bicycle.controller;
 
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import trokhimchuk.bicycle.exeptions.NotFoundException;
 
@@ -11,52 +13,43 @@ import java.util.Map;
 @RestController
 @RequestMapping("bicycle")
 public class BicycleController {
-    private int counter = 4;
 
-    public List<Map<String, String>> bicycles = new ArrayList<>(){{
-        add(new HashMap<String, String>() {{put("id", "1"); put("text", "one bike"); put("firm", "nike"); }});
-        add(new HashMap<String, String>() {{put("id", "2"); put("text", "two bike"); }});
-        add(new HashMap<String, String>() {{put("id", "3"); put("text", "three bike"); }});
-    }};
+
+    final BicycleRepository bicycleRepo;
+
+    @Autowired
+    public BicycleController(BicycleRepository bicycleRepo) {
+        this.bicycleRepo = bicycleRepo;
+    }
 
     @GetMapping
-    public List<Map<String, String>> list(){
-        return bicycles;
+    public List<Bicycle> list() {
+        return bicycleRepo.findAll();
     }
 
     @GetMapping("{id}")
-    public Map<String, String> getBicycle(@PathVariable String id) {
-        return getBic(id);
-    }
-
-    private Map<String, String> getBic(@PathVariable String id){
-        return bicycles.stream()
-                .filter(bicycle -> bicycle.get("id").equals(id))
-                .findFirst()
-                .orElseThrow(NotFoundException::new);
-    }
-
-
-    @PostMapping
-    public Map<String, String> create(@RequestBody Map<String, String> bicycle){
-        bicycle.put("id", String.valueOf(counter++));
-        bicycles.add(bicycle);
-
+    public Bicycle getBicycle(@PathVariable("id") Bicycle bicycle) {
         return bicycle;
     }
+
+    @PostMapping
+    public Bicycle create(@RequestBody Bicycle bicycle) {
+        return bicycleRepo.save(bicycle);
+    }
+
     @PutMapping("{id}")
-    public Map<String, String> update(@PathVariable String id, @RequestBody Map<String, String> bicycle){
-        Map<String, String> bicycleFromDB = getBic(id);
-
-        bicycleFromDB.putAll(bicycle);
-        bicycleFromDB.put("id", id);
-        return bicycleFromDB;
+    public Bicycle update(@PathVariable("id") Bicycle bicycleFromDB,
+                          @RequestBody Bicycle bicycle) {
+        BeanUtils.copyProperties(bicycle, bicycleFromDB, "id");
+        return bicycleRepo.save(bicycleFromDB);
     }
+
     @DeleteMapping("{id}")
-    public void delete(@PathVariable String id){
-        Map<String, String> bic = getBic(id);
+    public void delete(@PathVariable("id") Bicycle bicycle) {
+        bicycleRepo.delete(bicycle);
 
-        bicycles.remove(bic);
     }
+
+}
 
 }
